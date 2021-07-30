ed4e2d24528e66a5f7ddeab99b8f2eb2fc16864d
Katrina Nguyen <katrinan@usc.edu>
2021-07-30 13:14:31 -0700

    initializing project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic_app_weight_tracker',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
